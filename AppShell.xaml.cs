13fbbf717315b7ca47ff8d57ee747bb331a30864
Senac <Senac@TBE015D25>
2025-03-07 21:37:41 -0300

    Adicionar arquivos de projeto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MinhaAgenda
+{
+    //tudo que não for mostrar pro usuário é aqui
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
